e261f839aa690a715f41a7a7c1c31477a9c15d04
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2024-10-28 10:53:28 +0530

    we add the require field in cart models

--- src/models/CardModel.js
+++ src/models/CardModel.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const CardSchema = new mongoose.Schema({
     HTMLCard: { type: String, required: true },
     JSONCard: { type: Object, required: true }, // To store the card JSON object
-    image : {type : String , require : true },
+    image : {type : String , required : true },
     userId: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 
