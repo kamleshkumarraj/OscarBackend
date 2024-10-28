dee56849d834d5a7c836b6b3eed3e3715bde1e2c
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2024-10-28 10:47:28 +0530

    We successfully added the image in our cart

--- src/models/CardModel.js
+++ src/models/CardModel.js
@@ -4,8 +4,10 @@ import mongoose from 'mongoose';
 const CardSchema = new mongoose.Schema({
     HTMLCard: { type: String, required: true },
     JSONCard: { type: Object, required: true }, // To store the card JSON object
+    image : {type : String , require : true },
     userId: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
+
 });
 
 const Card = mongoose.model('Card', CardSchema);
